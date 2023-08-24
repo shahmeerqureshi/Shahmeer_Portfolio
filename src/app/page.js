d37456571dea4141a4d5da9274826b4e0c3f4bc8
Shahmeer Qureshi <shahmeerqureshi98@Shahmeers-MacBook-Air.local>
2023-08-24 00:47:45 -0400

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
           </div>
 
           <div className="relative mx-auto bg-gradient-to-b from-emerald-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={deved} fill style/>
+            <Image src="/software-developer-copy.png" alt="Software Developer" fill style/>
           </div>
 
         </section>
